4ea162975d8d6ce1e48b2e6f564e5098e80ebc98
Abhay Bansal <abhaybansal19000@gmail.com>
2026-09-09 14:33:50 +0000

    leetcode: sync 1 solution(s)

---
+++ uncategorized/minimum-bishop-moves-to-reach-target.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minBishopMoves(vector<int>& source, vector<int>& target) {
+        // Different colors
+        if ((source[0] + source[1]) % 2 != 
+            (target[0] + target[1]) % 2) {
+            return -1;
+        }
+
+        // Same diagonal
+        if (abs(source[0] - target[0]) == 
+            abs(source[1] - target[1])) {
+            return 1;
+        }
+
+        return 2;
+    }
+};
